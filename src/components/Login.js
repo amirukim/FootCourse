fbb6b68e1fa5b0406b746025e671faec839cb771
Mohamad Amirul Hakim Bin Johari <76082504+amirukim@users.noreply.github.com>
2023-06-26 15:28:56 +0800

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
