d2f2a1d23c7b047fe85572a24db6b055cd686e3a
Mohamad Amirul Hakim Bin Johari <76082504+amirukim@users.noreply.github.com>
2023-06-26 15:26:02 +0800

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,6 +20,16 @@ const Login = () => {
     } catch (error) {
       if (error.response) {
         console.log(error.response.data.msg);
+          toast.error(error.response.data.msg, {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
       }
     }
   };
