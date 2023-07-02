9d34de56ee74568b659b095a86ad4da0befb01e7
Mohamad Amirul Hakim Bin Johari <76082504+amirukim@users.noreply.github.com>
2023-07-02 14:48:35 +0800

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
